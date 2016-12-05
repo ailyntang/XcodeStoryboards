33b11481736bb652f25de7fd798f5246fd5edda0
Ai-Lyn Tang <ailyntang@gmail.com>
2016-12-05 21:47:13 +1100

    Add code to change label when button is tapped

--- Storyboards/ViewController.swift
+++ Storyboards/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func didTapButton(_ sender: Any) {
+        myLabel.text = "Booyah"
     }
 
     override func didReceiveMemoryWarning() {
